1a56c4f50a1cd2d2cf9d5c3527ceda01db173421
Owen Smith <owen.smith@citrix.com>
2019-07-17 13:18:33 +0100

    Update cache_interface header
    Requires bumping the INF revision and revision table
    
    Signed-off-by: Owen Smith <owen.smith@citrix.com>

--- include/revision.h
+++ include/revision.h
@@ -43,6 +43,7 @@
     DEFINE_REVISION(0x0800000B,  1,  6,  2,  1),    \
     DEFINE_REVISION(0x0800000C,  1,  7,  2,  1),    \
     DEFINE_REVISION(0x0800000D,  1,  8,  2,  1),    \
-    DEFINE_REVISION(0x09000000,  1,  8,  2,  1)
+    DEFINE_REVISION(0x09000000,  1,  8,  2,  1),    \
+    DEFINE_REVISION(0x09000001,  2,  8,  2,  1)
 
 #endif  // _REVISION_H
