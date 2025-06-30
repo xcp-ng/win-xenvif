4608bc1b3a120af7314c06949de340cc60319f13
Owen Smith <owen.smith@cloud.com>
2025-06-30 11:40:24 +0100

    Use UNPLUG v3
    Uses UnplugReboot to request a reboot from xenbus_monitor, rather than
    writing values into an absolute registry path, which is a driver verifier
    violation during Server 2025 WHQL testing.
    Also removes the RequestKey from the INF file.
    
    Signed-off-by: Owen Smith <owen.smith@cloud.com>

--- src/xenvif/driver.h
+++ src/xenvif/driver.h
@@ -58,11 +58,6 @@ DriverGetSettingsKey(
     VOID
     );
 
-extern VOID
-DriverRequestReboot(
-    VOID
-    );
-
 typedef struct _XENVIF_PDO  XENVIF_PDO, *PXENVIF_PDO;
 typedef struct _XENVIF_FDO  XENVIF_FDO, *PXENVIF_FDO;
 
